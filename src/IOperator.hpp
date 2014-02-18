a268899cac6926c1d861c737cc3a2c0c143d6918
izissise <hugues.m@hotmail.fr>
2014-02-18 10:08:11 +0100

    Declared template class Operand

--- src/IOperator.hpp
+++ src/IOperator.hpp
@@ -11,11 +11,11 @@ class IOperand
         virtual int getPrecision() const = 0; // Renvoie la precision du type de l’instance
         virtual eOperandType getType() const = 0; // Renvoie le type de l’instance. Voir plus bas
 
-        virtual IOperand * operator+(const IOperand &rhs) const = 0; // Somme
-        virtual IOperand * operator-(const IOperand &rhs) const = 0; // Difference
-        virtual IOperand * operator*(const IOperand &rhs) const = 0; // Produit
-        virtual IOperand * operator/(const IOperand &rhs) const = 0; // Quotient
-        virtual IOperand * operator%(const IOperand &rhs) const = 0; // Modulo
+        virtual IOperand* operator+(const IOperand &rhs) const = 0; // Somme
+        virtual IOperand* operator-(const IOperand &rhs) const = 0; // Difference
+        virtual IOperand* operator*(const IOperand &rhs) const = 0; // Produit
+        virtual IOperand* operator/(const IOperand &rhs) const = 0; // Quotient
+        virtual IOperand* operator%(const IOperand &rhs) const = 0; // Modulo
 
         virtual ~IOperand() {}
 };
--- src/Operand.hpp
+++ src/Operand.hpp
@@ -1,6 +1,7 @@
 #ifndef OPERAND_HPP_INCLUDED
 #define OPERAND_HPP_INCLUDED
 
+#include <stdint-gcc.h>
 #include "IOperator.hpp"
 
 template<typename T>
@@ -13,37 +14,74 @@ public:
   virtual int getPrecision();
   virtual eOperandType getType();
 
-  virtual IOperand* operator+(const IOperand &rhs); // Somme
-  virtual IOperand* operator-(const IOperand &rhs); // Difference
-  virtual IOperand* operator*(const IOperand &rhs); // Produit
-  virtual IOperand* operator/(const IOperand &rhs); // Quotient
-  virtual IOperand* operator%(const IOperand &rhs); // Modulo
+  virtual IOperand* operator+(const IOperand &rhs) const; // Somme
+  virtual IOperand* operator-(const IOperand &rhs) const; // Difference
+  virtual IOperand* operator*(const IOperand &rhs) const; // Produit
+  virtual IOperand* operator/(const IOperand &rhs) const; // Quotient
+  virtual IOperand* operator%(const IOperand &rhs) const; // Modulo
 
 private:
   std::string _value;
 };
 
-template<>
-Operand::Operand(const std::string& value)
+template<typename T>
+Operand<T>::Operand(const std::string& value)
 {
   _value = value;
 }
 
-template<>
-Operand::~Operand()
+template<typename T>
+Operand<T>::~Operand()
 {
 }
 
-template<>
-int Operand::getPrecision()
+template<typename T>
+int Operand<T>::getPrecision()
 {
   return (sizeof(T));
 }
 
-template<>
-int Operand::getType()
+template<typename T>
+eOperandType Operand<T>::getType()
 {
   return (sizeof(T)); //not this
 }
+
+template<typename T>
+IOperand* Operand<T>::operator+(const IOperand &rhs) const
+{
+
+}
+
+template<typename T>
+IOperand* Operand<T>::operator-(const IOperand &rhs) const
+{
+
+}
+
+template<typename T>
+IOperand* Operand<T>::operator*(const IOperand &rhs) const
+{
+
+}
+
+template<typename T>
+IOperand* Operand<T>::operator/(const IOperand &rhs) const
+{
+
+}
+
+template<typename T>
+IOperand* Operand<T>::operator%(const IOperand &rhs) const
+{
+
+}
+
+typedef Operand<int8_t> Int8;
+typedef Operand<int16_t> Int16;
+typedef Operand<int32_t> Int32;
+typedef Operand<int64_t> Int64;
+typedef Operand<float> Float;
+typedef Operand<double> Double;
 
 #endif // OPERAND_HPP_INCLUDED
