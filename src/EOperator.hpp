9a01a9d65097dabd13d98693bdfdbf0839a06d99
Maxime <maxime@lavand-m-PC.(none)>
2014-02-18 09:56:08 +0100

    add enum and createoperand

---
+++ src/EOperator.hpp
@@ -0,0 +1,6 @@
+#ifndef EOPERATOR_HPP_INCLUDED
+#define EOPERATOR_HPP_INCLUDED
+
+enum eOperandType{Int8,Int16,Int32,Float,Double};
+
+#endif // EOPERATOR_HPP_INCLUDED
---
+++ src/cpu.h
@@ -0,0 +1,19 @@
+#ifndef CPU_H
+#define CPU_H
+
+
+class cpu
+{
+	public:
+		cpu();
+		~cpu();
+		IOperand *cpu::createOperand(eOperandType type, const std::string & value);
+	private:
+		IOperand * createInt8(const std::string & value);
+		IOperand * createInt16(const std::string & value);
+		IOperand * createInt32(const std::string & value);
+		IOperand * createFloat(const std::string & value);
+		IOperand * createDouble(const std::string & value);
+};
+
+#endif // CPU_H
