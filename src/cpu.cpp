c0f1ef4087ddb599bf4f7cd5ef50a12a151c5ba4
Maxime <maxime@lavand-m-PC.(none)>
2014-02-18 10:22:10 +0100

    add :: for enum

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -39,10 +39,10 @@ IOperand *Cpu::createOperand(eOperandType type, const std::string & value)
 {
     std::map<eOperandType,IOperand*(*)(const std::string &)> createType;
 
-    createType[Int8]=&createInt8;
-    createType[Int16]=&createInt16;
-    createType[Int32]=&createInt32;
-    createType[Float]=&createFloat;
-    createType[Double]=&createDouble;
+    createType[::Int8]=&createInt8;
+    createType[::Int16]=&createInt16;
+    createType[::Int32]=&createInt32;
+    createType[::Float]=&createFloat;
+    createType[::Double]=&createDouble;
     return(createType[type](value));
 }
