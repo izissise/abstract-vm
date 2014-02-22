5facb848f48b3e4154d862f3dfaac38e467a6b6d
izissise <hugues.m@hotmail.fr>
2014-02-22 17:48:17 +0100

    Add a test

--- src/EOperator.hpp
+++ src/EOperator.hpp
@@ -1,6 +1,13 @@
 #ifndef EOPERATOR_HPP_INCLUDED
 #define EOPERATOR_HPP_INCLUDED
 
-enum eOperandType{Int8,Int16,Int32,Float,Double};
+enum eOperandType
+{
+  Int8 = 0,
+  Int16,
+  Int32,
+  Float,
+  Double
+};
 
 #endif // EOPERATOR_HPP_INCLUDED
